dc8c12943e6835fc20de9629a2b2d821c32ba5c5
Dam58 <damiano.mereu.85@gmail.com>
2019-10-18 12:03:40 +0000

    ADD file FeatureUserManagment.sql

---
+++ FeatureUserManagment.sql
@@ -0,0 +1,34 @@
+-- TABLE
+CREATE TABLE Bl_AccountManagement_Identificative_User
+          (
+           ID_Identificative_User INTEGER PRIMARY KEY AUTOINCREMENT,
+           Username VARCHAR2(25) NOT NULL,
+           Password VARCHAR2(25) NOT NULL,
+  FK_ID_User INTEGER NOT NULL -- Chiave esterna tabella User
+          );
+CREATE TABLE Bl_AccountManagement_Identificative_User_Privilege
+          (
+           ID_Identificative_User_Privilege INTEGER PRIMARY KEY AUTOINCREMENT,
+           FK_ID_Privilege INTEGER NOT NULL,
+           FK_ID_Identificative_User INTEGER NOT NULL
+         );
+CREATE TABLE Bl_AccountManagement_Privilege
+          (
+           ID_Privilege INTEGER PRIMARY KEY AUTOINCREMENT,
+           Type_Privilege VARCHAR2(25) NOT NULL
+          );
+CREATE TABLE Bl_AccountManagement_User
+          (
+           ID_User INTEGER PRIMARY KEY AUTOINCREMENT,
+           Name VARCHAR2(25) NOT NULL,
+           Surname VARCHAR2(25) NOT NULL,
+           Birth_year DATE NOT NULL
+          );
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
